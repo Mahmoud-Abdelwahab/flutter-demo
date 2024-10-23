de18da09b6b656f309321e21bd14bac1ad9c4e80
Mahmoud abdelwahab <mahmoud.elziany@gmail.com>
2024-10-23 14:33:07 +0300

    adding header

--- lib/messenger_view.dart
+++ lib/messenger_view.dart
@@ -8,16 +8,64 @@ class MessengerView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: CircleAvatar(
-          backgroundImage: NetworkImage(
-              'https://example.com/your-image-url.jpg'), // Replace with your image URL radius:20.0, // Adjust radius as needed ),
-        ),
-        title:  IconButton(
-          onPressed: () {},
-          icon: const Icon(Icons.edit),
+       title: const Row(
+         children: [
+           CircleAvatar(
+             radius: 20,
+             backgroundImage: NetworkImage("https://avatars.githubusercontent.com/u/17813115?v=4"),
+           ),
+           SizedBox(
+             width: 10,
+           ),
+           Text("Chats",
+             style: TextStyle(
+               color: Colors.black,
+               fontSize: 20,
+               fontWeight: FontWeight.w500,
+             ),
+           ),
+         ],
+       ),
+        actions: [
+          IconButton(onPressed: (){},
+              icon: const CircleAvatar(
+                backgroundColor: Colors.black26,
+                radius: 16,
+                child: Icon(Icons.camera),
+              )),
+          IconButton(onPressed: (){},
+              icon: const CircleAvatar(
+                backgroundColor: Colors.black26,
+                radius: 16,
+                child: Icon(Icons.edit),
+              )),
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            GestureDetector(
+              onTap: (){
+                debugPrint("Search Tapped.");
+              },
+              child: Container(
+                padding: EdgeInsets.all(7),
+               decoration: BoxDecoration(
+                 color: Colors.grey[300],
+                 borderRadius: BorderRadius.circular(5.0),
+               ),
+               child: const Row(
+                 children: [
+                   Icon(Icons.search),
+                   Text('Search'),
+                 ],
+               ),
+              ),
+            ),
+          ],
         ),
       ),
-      body: Scaffold(),
     );
   }
 }
