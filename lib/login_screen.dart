a1195ac4134ba5be5e2ea3de83d6288103ace053
Mahmoud abdelwahab <mahmoud.elziany@gmail.com>
2024-10-23 04:01:29 +0300

    finalizing login screen

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -12,73 +12,77 @@ var passwordText = TextEditingController();
       appBar: AppBar(),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Login",
-              style: TextStyle(
-                fontSize: 45,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              keyboardType: TextInputType.emailAddress,
-            controller: emailText,
-             onChanged: (text) {
-                print(text);
-             },
-             decoration: const InputDecoration(
-               labelText: "Enter your email.",
-               prefixIcon: Icon(Icons.email),
-               border: OutlineInputBorder(),
-             ),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            TextFormField(
-              controller: passwordText,
-              keyboardType: TextInputType.visiblePassword,
-              obscureText: true,
-              decoration: const InputDecoration(
-                labelText: "Enter your password",
-                prefixIcon: Icon(Icons.lock),
-                suffixIcon: Icon(Icons.remove_red_eye),
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            Container(
-              width: double.infinity,
-              color: Colors.blue,
-              child: MaterialButton(onPressed: (){
-               print(emailText.text);
-               print(passwordText.text);
-              },
-              child:
-              const Text("LOGIN",
-                style: TextStyle(color: Colors.white),
-              ),
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text("Don\'t have an account?"),
-                TextButton(onPressed: (){
-                  print("Create account");
-                },
-                    child: Text("Register Now")
+                Text(
+                  "Login",
+                  style: TextStyle(
+                    fontSize: 45,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                controller: emailText,
+                 onChanged: (text) {
+                    print(text);
+                 },
+                 decoration: const InputDecoration(
+                   labelText: "Enter your email.",
+                   prefixIcon: Icon(Icons.email),
+                   border: OutlineInputBorder(),
+                 ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                  controller: passwordText,
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    labelText: "Enter your password",
+                    prefixIcon: Icon(Icons.lock),
+                    suffixIcon: Icon(Icons.remove_red_eye),
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Container(
+                  width: double.infinity,
+                  color: Colors.blue,
+                  child: MaterialButton(onPressed: (){
+                   print(emailText.text);
+                   print(passwordText.text);
+                  },
+                  child:
+                  const Text("LOGIN",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Don\'t have an account?"),
+                    TextButton(onPressed: (){
+                      print("Create account");
+                    },
+                        child: Text("Register Now")
+                    )
+                  ],
                 )
               ],
-            )
-          ],
+            ),
+          ),
         ),
       ),
     );
