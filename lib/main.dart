61ebcb5609bf6f5dcd5cbc87b5e3dea6e0d55989
Mahmoud abdelwahab <mahmoud.elziany@gmail.com>
2024-10-24 11:41:36 +0300

    using list view with separators in messenger

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: MessengerView(),
     );
   }
--- lib/messenger_view.dart
+++ lib/messenger_view.dart
@@ -8,32 +8,36 @@ class MessengerView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-       title: const Row(
-         children: [
-           CircleAvatar(
-             radius: 20,
-             backgroundImage: NetworkImage("https://avatars.githubusercontent.com/u/17813115?v=4"),
-           ),
-           SizedBox(
-             width: 10,
-           ),
-           Text("Chats",
-             style: TextStyle(
-               color: Colors.black,
-               fontSize: 20,
-               fontWeight: FontWeight.w500,
-             ),
-           ),
-         ],
-       ),
+        title: const Row(
+          children: [
+            CircleAvatar(
+              radius: 20,
+              backgroundImage: NetworkImage(
+                  "https://avatars.githubusercontent.com/u/17813115?v=4"),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Text(
+              "Chats",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ],
+        ),
         actions: [
-          IconButton(onPressed: (){},
+          IconButton(
+              onPressed: () {},
               icon: const CircleAvatar(
                 backgroundColor: Colors.black26,
                 radius: 16,
                 child: Icon(Icons.camera),
               )),
-          IconButton(onPressed: (){},
+          IconButton(
+              onPressed: () {},
               icon: const CircleAvatar(
                 backgroundColor: Colors.black26,
                 radius: 16,
@@ -42,138 +46,163 @@ class MessengerView extends StatelessWidget {
         ],
       ),
       body: Padding(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            GestureDetector(
-              onTap: (){
-                debugPrint("Search Tapped.");
-              },
-              child: Container(
-                padding: EdgeInsets.all(7),
-               decoration: BoxDecoration(
-                 color: Colors.grey[300],
-                 borderRadius: BorderRadius.circular(5.0),
-               ),
-               child: const Row(
-                 children: [
-                   Icon(Icons.search),
-                   Text('Search'),
-                 ],
-               ),
-              ),
-            ),
-            const SizedBox(
-              height: 25,
-            ),
-            Container(
-              width: 60,
-              child: const Column(
-                children: [
-                  Stack(
-                    alignment: AlignmentDirectional.bottomEnd,
+        padding: const EdgeInsetsDirectional.only(
+          bottom: 20,
+          start: 20,
+          end: 20,
+        ),
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.only(top: 15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              GestureDetector(
+                onTap: () {
+                  debugPrint("Search Tapped.");
+                },
+                child: Container(
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                  child: const Row(
                     children: [
-                      CircleAvatar(
-                        radius: 25,
-                        backgroundImage: NetworkImage("https://avatars.githubusercontent.com/u/17813115?v=4"),
-                      ),
-                      Padding(
-                        padding: EdgeInsetsDirectional.only(
-                          end: 3,
-                          bottom: 3,
-                        ),
-                        child: CircleAvatar(
-
-                          radius: 7,
-                          backgroundColor: Colors.green,
-                        ),
-                      ),
+                      Icon(Icons.search),
+                      Text('Search'),
                     ],
                   ),
-                  SizedBox(
-                    height: 7,
-                  ),
-                  Text('Mahmoud abdelwahab',
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                      fontSize: 12,
-                      color: Colors.black,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-          Row(
-            children: [
-              Stack(
-                alignment: AlignmentDirectional.bottomEnd,
-                children: [
-                  CircleAvatar(
-                    radius: 25,
-                    backgroundImage: NetworkImage("https://avatars.githubusercontent.com/u/17813115?v=4"),
-                  ),
-                  Padding(
-                    padding: EdgeInsetsDirectional.only(
-                      end: 3,
-                      bottom: 3,
-                    ),
-                    child: CircleAvatar(
-
-                      radius: 7,
-                      backgroundColor: Colors.green,
-                    ),
-                  ),
-                ],
+                ),
               ),
               const SizedBox(
-                width: 10,
+                height: 25,
               ),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                        'Mahmoud Abdulwahab',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Row(
-                     children: [
-                       const Expanded(child:
-                       Text(
-                           'Okay i will be there ffdfsdfsdfsfddsf',
-                         maxLines: 1,
-                         overflow: TextOverflow.ellipsis,
-                       )
-                       ),
-                       Padding(
-                         padding: const EdgeInsetsDirectional.symmetric(horizontal: 5),
-                         child: Container(
-                           width: 6,
-                           height: 6,
-                          decoration: const BoxDecoration(
-                            color: Colors.blue,
-                            shape: BoxShape.circle,
-                          ),
-                         ),
-                       ),
-                       Text('02:30 PM'),
-                     ],
-                    ),
-                  ],
-                ),
-              )
+              Container(
+                height: 100,
+                child: ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) => buildStoryItem(),
+                    separatorBuilder: (context, index) => const SizedBox(
+                          width: 10,
+                        ),
+                    itemCount: 10),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              ListView.separated(
+                shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemBuilder: (context, index) => buildChatItem(),
+                  separatorBuilder: (context, index) => const Divider(),
+                  itemCount: 20),
             ],
           ),
-          ],
         ),
       ),
     );
   }
+
+  Widget buildStoryItem() => Container(
+        width: 60,
+        child: const Column(
+          children: [
+            Stack(
+              alignment: AlignmentDirectional.bottomEnd,
+              children: [
+                CircleAvatar(
+                  radius: 25,
+                  backgroundImage: NetworkImage(
+                      "https://avatars.githubusercontent.com/u/17813115?v=4"),
+                ),
+                Padding(
+                  padding: EdgeInsetsDirectional.only(
+                    end: 3,
+                    bottom: 3,
+                  ),
+                  child: CircleAvatar(
+                    radius: 7,
+                    backgroundColor: Colors.green,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 7,
+            ),
+            Text(
+              'Mahmoud abdelwahab',
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                fontSize: 12,
+                color: Colors.black,
+              ),
+            ),
+          ],
+        ),
+      );
+  Widget buildChatItem() => Row(
+        children: [
+          Stack(
+            alignment: AlignmentDirectional.bottomEnd,
+            children: [
+              CircleAvatar(
+                radius: 25,
+                backgroundImage: NetworkImage(
+                    "https://avatars.githubusercontent.com/u/17813115?v=4"),
+              ),
+              Padding(
+                padding: EdgeInsetsDirectional.only(
+                  end: 3,
+                  bottom: 3,
+                ),
+                child: CircleAvatar(
+                  radius: 7,
+                  backgroundColor: Colors.green,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Mahmoud Abdulwahab',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Row(
+                  children: [
+                    const Expanded(
+                        child: Text(
+                      'Okay i will be there ffdfsdfsdfsfddsf',
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    )),
+                    Padding(
+                      padding:
+                          const EdgeInsetsDirectional.symmetric(horizontal: 5),
+                      child: Container(
+                        width: 6,
+                        height: 6,
+                        decoration: const BoxDecoration(
+                          color: Colors.blue,
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                    ),
+                    Text('02:30 PM'),
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
+      );
 }
