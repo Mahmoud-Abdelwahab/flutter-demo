7966f53e339a1d51ca123ee20666f1e8d3d5b362
Mahmoud abdelwahab <mahmoud.elziany@gmail.com>
2024-10-28 07:18:52 +0300

    using form validator

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 class LoginScreen extends StatelessWidget {
   LoginScreen({super.key});
 
-var emailText = TextEditingController();
-var passwordText = TextEditingController();
-
+  var emailText = TextEditingController();
+  var passwordText = TextEditingController();
+  var formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,73 +14,92 @@ var passwordText = TextEditingController();
         padding: const EdgeInsets.all(16.0),
         child: Center(
           child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  "Login",
-                  style: TextStyle(
-                    fontSize: 45,
-                    fontWeight: FontWeight.w600,
+            child: Form(
+              key: formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Login",
+                    style: TextStyle(
+                      fontSize: 45,
+                      fontWeight: FontWeight.w600,
+                    ),
                   ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  keyboardType: TextInputType.emailAddress,
-                controller: emailText,
-                 onChanged: (text) {
-                    print(text);
-                 },
-                 decoration: const InputDecoration(
-                   labelText: "Enter your email.",
-                   prefixIcon: Icon(Icons.email),
-                   border: OutlineInputBorder(),
-                 ),
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                TextFormField(
-                  controller: passwordText,
-                  keyboardType: TextInputType.visiblePassword,
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                    labelText: "Enter your password",
-                    prefixIcon: Icon(Icons.lock),
-                    suffixIcon: Icon(Icons.remove_red_eye),
-                    border: OutlineInputBorder(),
+                  const SizedBox(
+                    height: 20,
                   ),
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                Container(
-                  width: double.infinity,
-                  color: Colors.blue,
-                  child: MaterialButton(onPressed: (){
-                   print(emailText.text);
-                   print(passwordText.text);
-                  },
-                  child:
-                  const Text("LOGIN",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Don\'t have an account?"),
-                    TextButton(onPressed: (){
-                      print("Create account");
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return " email must not br empty";
+                      }
+                      return null;
                     },
-                        child: Text("Register Now")
-                    )
-                  ],
-                )
-              ],
+                    controller: emailText,
+                    onChanged: (text) {
+                      print(text);
+                    },
+                    decoration: const InputDecoration(
+                      labelText: "Enter your email.",
+                      prefixIcon: Icon(Icons.email),
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  TextFormField(
+                    controller: passwordText,
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: true,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return " password is too short";
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                      labelText: "Enter your password",
+                      prefixIcon: Icon(Icons.lock),
+                      suffixIcon: Icon(Icons.remove_red_eye),
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Container(
+                    width: double.infinity,
+                    color: Colors.blue,
+                    child: MaterialButton(
+                      onPressed: () {
+                        if (formKey.currentState != null &&
+                            formKey.currentState!.validate()) {
+                          print(emailText.text);
+                          print(passwordText.text);
+                        }
+                      },
+                      child: const Text(
+                        "LOGIN",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Don\'t have an account?"),
+                      TextButton(
+                          onPressed: () {
+                            print("Create account");
+                          },
+                          child: Text("Register Now"))
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
