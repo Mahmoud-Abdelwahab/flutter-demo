b4fb710a7fc3b6b4e1117ee695b6db3da5737a9a
Mahmoud abdelwahab <mahmoud.elziany@gmail.com>
2024-10-28 10:27:26 +0300

    init tasks

---
+++ lib/moduls/new_tasks.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Tasks extends StatelessWidget {
+  const Tasks({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Text(
+            'Tasks'
+        )
+    );
+  }
+}
