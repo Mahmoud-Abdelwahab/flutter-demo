509e49e0d7594c342bd192b11c8965e9f27abcc8
Mahmoud abdelwahab <mahmoud.elziany@gmail.com>
2024-10-28 10:27:05 +0300

    adding archived tasks

---
+++ lib/moduls/archieved_tasks.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ArchievedTasks extends StatelessWidget {
+  const ArchievedTasks({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Text(
+            'Tasks'
+        )
+    );
+  }
+}
